fb294d97092da80dae7551a952e42c54335a9a2a
M <M@10.31.202.106>
2014-05-05 15:14:47 +0200

    leck mich

--- src/Square.java
+++ src/Square.java
@@ -4,43 +4,44 @@ public class Square {
 	public int row;
 	
 	Square(String command){
-		char row = command.charAt(0);
-		this.col = Integer.getInteger(command);
-		if(row == 'a'){
-			this.row = 1;
-		}else if(row == 'b'){
-			this.row = 2;
-		}else if(row == 'c'){
-			this.row = 3;
-		}else if(row == 'd'){
-			this.row = 4;
-		}else if(row == 'e'){
-			this.row = 5;
-		}else if(row == 'f'){
-			this.row = 6;
+		this.row = Integer.getInteger(command);
+		char col = command.charAt(0);
+		if(col == 'a' || col == 'A'){
+			this.col = 0;
+		}else if(col == 'b' || col == 'B'){
+			this.col = 1;
+		}else if(col == 'c' || col == 'C'){
+			this.col = 2;
+		}else if(col == 'd' || col == 'D'){
+			this.col = 3;
+		}else if(col == 'e' || col == 'E'){
+			this.col = 4;
+		}else if(col == 'f' || col == 'F'){
+			this.col = 5;
 		}
 	}
 	
 	Square(int col, int row){
 		this.col = col;
-		this.row = row;
+		this.row = row-1;
 	}
 	
 	public String toString(){
-		char row = '\u0000';
-		
-		if(row == 1){
-			row = 'a';
-		}else if(row == 2){
-			row = 'b';
-		}else if(row == 3){
-			row = 'c';
-		}else if(row == 4){
-			row = 'd';
-		}else if(row == 5){
-			row = 'e';
-		}else if(row == 6){
-			row = 'f';
+		char col = '\u0000';
+		if(col == 0){
+			col = 'a';
+		}else if(col == 1){
+			col = 'b';
+		}else if(col == 2){
+			col = 'c';
+		}else if(col == 3){
+			col = 'd';
+		}else if(col == 4){
+			col = 'e';
+		}else if(col == 5){
+			col = 'f';
 		}
+		return col+Integer.toString(row+1);
+		
 	}
 }
