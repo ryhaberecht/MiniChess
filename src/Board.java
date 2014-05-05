0962a8046aee6296102ba5dde74a568457220d96
ente <ente@lappi>
2014-05-05 11:10:58 +0200

    kommentar und toString()

--- src/Board.java
+++ src/Board.java
@@ -3,8 +3,27 @@ public class Board
 {
 	char[][] squares = new char[6][5];	// number / letter -> row / column
 	
-	int moveNumber;
+	int moveNumber;	// 1 to 40 (40 = draw)
 	
-	char whoseMove;
+	char whoseMove;	// B or W
+	
+	
+	public Board(String boardString)
+	{
+		
+	}
+	
+	public String toString()
+	{
+		String boardString = "";
+		
+		for (int i = 0; i < 6; i++) {
+			for (int j = 0; j < 5; j++) {
+				boardString += squares[i][j];
+			}
+			boardString += "\n";
+		}
+		
+		return boardString += moveNumber + " " + whoseMove;
+	}
 }
-
