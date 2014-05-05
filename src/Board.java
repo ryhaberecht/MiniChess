368a1f4c7faef6780dca6e0b3f73a3196ea97ec8
M <M@10.31.202.106>
2014-05-05 15:56:27 +0200

    clean up

--- src/Board.java
+++ src/Board.java
@@ -1,6 +1,4 @@
-import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.InputStreamReader;
 import java.io.Reader;
 import java.io.Writer;
 
@@ -121,6 +119,7 @@ public class Board {
 		}
 	}
 
+	@Override
 	public String toString() {
 		String result = moveNum + " " + onMove + "\n";
 		for (int row = 5; row >= 0; row--) {
