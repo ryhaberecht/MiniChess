28bd23286e70699406f36a3758ccab4aa128acc9
ente <ente@lappi>
2014-05-06 18:03:37 +0200

    added telnet client

--- src/Constants.java
+++ src/Constants.java
@@ -4,4 +4,8 @@ public final class Constants
 	public final static short MAX_ROW = 5;
 	public final static short MIN_COLUMN = 0;
 	public final static short MAX_COLUMN = 4;
+	public final static String SERVER = "imcs.svcs.cs.pdx.edu";
+	public final static String PORT = "3589";
+	public final static String USERNAME = "MiniCheese2014";
+	public final static String PASSWORD = "appenzeller";
 }
--- src/Driver.java
+++ src/Driver.java
@@ -11,19 +11,19 @@ public class Driver
 		System.out.println("Which kind of game? (human-human/human-ai/ai-ai/ai-telnet):");
 		Scanner scanIn = new Scanner(System.in);
 		String gameType = scanIn.nextLine().trim();
-		
+
 		// initialize varaibles
-		Board board = new Board("11 W\nkqbnr\npp.pp\n..p.P\n.....\nPPPPN\nR.BQK");
+		Board board = new Board();
 		String move;
 		char winCondition;
-		
-		if ("human-human".matches(gameType)) {	// human on human
-			
+
+		if ("human-human".matches(gameType)) { // human on human
+
 			System.out.println(board.toHumanReadableString());
 			System.out.println();
 			System.out.println(board.printValidMovesForNextTurn());
 			System.out.println("Next move:");
-			
+
 			do {
 				move = scanIn.nextLine();
 				winCondition = board.checkedMove(new Move(move));
@@ -54,10 +54,10 @@ public class Driver
 			while (scanIn.hasNextLine());
 			scanIn.close();
 		}
-		else if ("ai-ai".matches(gameType)) {	// ai on ai
-			
+		else if ("ai-ai".matches(gameType)) { // ai on ai
+
 			Writer writer = new FileWriter("output.txt");
-			
+
 			writer.write(board.toHumanReadableString());
 			writer.write("\n");
 			writer.write("\n");
@@ -104,8 +104,8 @@ public class Driver
 			while (true);
 			writer.close();
 		}
-		else if ("human-ai".matches(gameType)) {	// human on ai
-			
+		else if ("human-ai".matches(gameType)) { // human on ai
+
 			System.out.println(board.toHumanReadableString());
 			System.out.println();
 			System.out.println(board.printValidMovesForNextTurn());
@@ -116,14 +116,14 @@ public class Driver
 			char humanColorChar = humanColor.trim().charAt(0);
 			do {
 				// is it human's or ai's turn?
-				if (humanColorChar == board.onMove) {	// human is next
+				if (humanColorChar == board.onMove) { // human is next
 					move = scanIn.nextLine();
 					winCondition = board.checkedMove(new Move(move));
 				}
-				else {	// ai is next
+				else { // ai is next
 					winCondition = board.move(getAiMove(board));
 				}
-				
+
 				System.out.println();
 				System.out.println(board.toHumanReadableString());
 				System.out.println();
@@ -151,66 +151,123 @@ public class Driver
 			while ((board.onMove != humanColorChar) | scanIn.hasNextLine());
 			scanIn.close();
 		}
-		else if ("ai-telnet".matches(gameType)) {	// ai on telnet TODO
-			
-			System.out.println(board.toHumanReadableString());
-			System.out.println();
-			System.out.println(board.printValidMovesForNextTurn());
-			System.out.println("Next move:");
+		else if ("ai-telnet".matches(gameType)) { // ai on telnet
 
-			System.out.println("Choose your color: ");
-			String humanColor = scanIn.nextLine();
-			char humanColorChar = humanColor.trim().charAt(0);
-			do {
-				// is it human's or ai's turn?
-				if (humanColorChar == board.onMove) {	// human is next
-					move = scanIn.nextLine();
-					winCondition = board.checkedMove(new Move(move));
+			// create connection to ICMS
+			System.out.println("Trying to connect to ICMS server...");
+			TelnetClient telnetClient = new TelnetClient(Constants.SERVER, Constants.PORT, Constants.USERNAME, Constants.PASSWORD);
+
+			// delete all old offers
+			telnetClient.send("clean", false);
+
+			// offer or accept game
+			System.out.println("Offer (o) or accept (a) game?");
+			char offerOrAccept = scanIn.nextLine().charAt(0);
+			char colorToPlay;
+
+			if (offerOrAccept == 'o') { // offer
+
+				System.out.println("Choose color (W/B/?):");
+				char chosenColor = scanIn.nextLine().charAt(0);
+				colorToPlay = telnetClient.offer(chosenColor);
+			}
+			else if (offerOrAccept == 'a') { // accept
+
+				System.out.println("Accept game nr.:");
+				String gameNr = scanIn.nextLine();
+				System.out.println("Choose color (W/B/?):");
+				char chosenColor = scanIn.nextLine().charAt(0);
+				colorToPlay = telnetClient.accept(gameNr, chosenColor);
+			}
+			else { // wrong input
+				telnetClient.close();
+				scanIn.close();
+				throw new Error("Action does not exist!");
+			}
+
+			// print initial board TODO optimize
+			System.out.println("\n" + board.toHumanReadableString() + "\n");
+			System.out.println(board.printValidMovesForNextTurn()+"\nNext move:");
+			
+			// maybe wait for adversary
+			String adversaryMove;
+			Move ownMove;
+			if (colorToPlay == 'B') { // start second, adversary is White
+				adversaryMove = telnetClient.getMove(); // wait for adversary's move
+				if (adversaryMove == null) {
+					telnetClient.close();
+					scanIn.close();
+					throw new Error("game or connection ended before first move of adversary.");
 				}
-				else {	// ai is next
-					winCondition = board.move(getAiMove(board));
-				}
-				
-				System.out.println();
-				System.out.println(board.toHumanReadableString());
-				System.out.println();
+				winCondition = board.move(new Move(adversaryMove));
+				//TODO optimize
+				System.out.println(adversaryMove+"\n\n" + board.toHumanReadableString() + "\n");
+				System.out.println(board.printValidMovesForNextTurn()+"\nNext move:");
 				if (winCondition == '=') { // tie
 					System.out.println("Tie! Nobody wins.");
-					break;
 				}
 				else if (winCondition == 'W') { // white wins
 					System.out.println("White wins!");
-					break;
 				}
 				else if (winCondition == 'B') { // black wins
 					System.out.println("Black wins!");
-					break;
-				}
-				else if (winCondition == '?') { // nobody won yet
-					System.out.println(board.printValidMovesForNextTurn());
-					System.out.println("Next move:");
-				}
-				else { // invalid win condition
-					scanIn.close();
-					throw new Error("Invalid win condition returned by move()");
 				}
 			}
-			while ((board.onMove != humanColorChar) | scanIn.hasNextLine());
-			scanIn.close();
+
+			while (true) {
+
+				// make own move
+				ownMove = getAiMove(board);
+				telnetClient.sendMove(ownMove.toString());
+				winCondition = board.move(ownMove);
+				//TODO optimize
+				System.out.println(ownMove+"\n\n" + board.toHumanReadableString() + "\n");
+				System.out.println(board.printValidMovesForNextTurn()+"\nNext move:");
+				if (winCondition == '=') { // tie
+					System.out.println("Tie! Nobody wins.");
+				}
+				else if (winCondition == 'W') { // white wins
+					System.out.println("White wins!");
+				}
+				else if (winCondition == 'B') { // black wins
+					System.out.println("Black wins!");
+				}
+
+				// wait for adversary
+				adversaryMove = telnetClient.getMove(); // wait for adversary's move
+				if (adversaryMove == null) {
+					break;
+				}
+				winCondition = board.move(new Move(adversaryMove));
+				//TODO optimize
+				System.out.println(adversaryMove+"\n\n" + board.toHumanReadableString() + "\n");
+				System.out.println(board.printValidMovesForNextTurn()+"\nNext move:");
+				if (winCondition == '=') { // tie
+					System.out.println("Tie! Nobody wins.");
+				}
+				else if (winCondition == 'W') { // white wins
+					System.out.println("White wins!");
+				}
+				else if (winCondition == 'B') { // black wins
+					System.out.println("Black wins!");
+				}
+			}
+			telnetClient.close(); // close ICMS connection
 		}
-		else {	// unknown game type
+		else { // unknown game type
 			scanIn.close();
 			throw new Error("Unknown game type: " + gameType);
 		}
+		scanIn.close();
 	}
 
 	private static Move getRandomAiMove(Board board)
 	{
 		int listLength = board.legalMovesForNextTurn.size();
-		int randomIndex = (int)(Math.random() * listLength);
+		int randomIndex = (int) (Math.random() * listLength);
 		return board.legalMovesForNextTurn.get(randomIndex);
 	}
-	
+
 	private static Move getAiMove(Board board)
 	{
 		return getRandomAiMove(board);
