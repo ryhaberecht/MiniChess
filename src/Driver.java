940c581dda394f9f44d405ff68988fb82c9ac76f
ente <ente@lappi>
2014-05-06 12:13:44 +0200

    check human moves, do not check 'normal' moves. etc.

--- src/Driver.java
+++ src/Driver.java
@@ -4,20 +4,41 @@ public class Driver
 {
 	public static void main(String[] args)
 	{
-		Board board = new Board("11 W\nkqbnr\npp.pp\n..p.P\n.....\nPPPPN\nR.BQK");
+		Board board = new Board("39 W\nkqbnr\npp.pp\n..p.P\n.....\nPPPPN\nR.BQK");
 		System.out.println(board.toHumanReadableString());
 		System.out.println();
+		System.out.println(board.printValidMovesForNextTurn());
 		System.out.println("Next move:");
 
 		String move;
+		char winCondition;
 		Scanner scanIn = new Scanner(System.in);
 		do {
 			move = scanIn.nextLine();
-			board.move(new Move(move));
+			winCondition = board.checkedMove(new Move(move));
 			System.out.println();
 			System.out.println(board.toHumanReadableString());
 			System.out.println();
-			System.out.println("Next move:");
+			if (winCondition == '=') {	// tie
+				System.out.println("Tie! Nobody wins.");
+				break;
+			}
+			else if (winCondition == 'W') {	// white wins
+				System.out.println("White wins!");
+				break;
+			}
+			else if (winCondition == 'B') {	// black wins
+				System.out.println("Black wins!");
+				break;
+			}
+			else if (winCondition == '?') {	// nobody won yet
+				System.out.println(board.printValidMovesForNextTurn());
+				System.out.println("Next move:");
+			}
+			else {	// invalid win condition
+				scanIn.close();
+				throw new Error("Invalid win condition returned by move()");
+			}
 		}
 		while (scanIn.hasNextLine());
 		scanIn.close();
