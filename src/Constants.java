3526d6fc5787b9dcb98945a394d06274c9a41abf
ente <ente@lappi>
2014-05-08 16:49:13 +0200

    a

--- src/Constants.java
+++ src/Constants.java
@@ -8,5 +8,5 @@ public final class Constants
 	public final static String PORT = "21";
 	public final static String USERNAME = "MiniCheese";
 	public final static String PASSWORD = "appenzeller";
-	public final static int MILLISECONDS_PER_MOVE = 6000;
+	public final static int MILLISECONDS_PER_MOVE = 7000;
 }
